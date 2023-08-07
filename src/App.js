eba061401f5c724895e2bb37f45b7c0ddc315b6a
Qibin <qbye0405@gmail.com>
2023-08-07 12:04:23 -0400

    optimized category div

--- src/App.js
+++ src/App.js
@@ -1,43 +1,39 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  const categories = [
+    {
+      id: 1,
+      title: "HATS",
+    },
+    {
+      id: 2,
+      title: "JACKETS",
+    },
+    {
+      id: 3,
+      title: "SNEAKERS",
+    },
+    {
+      id: 4,
+      title: "WOMENS",
+    },
+    {
+      id: 5,
+      title: "MENS",
+    },
+  ];
   return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/*<img /> */}
-        <div className='category-body-container'>
-          <h2>HATS</h2>
-          <p>SHOP NOW</p>
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          {/*<img /> */}
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>SHOP NOW</p>
+          </div>
         </div>
-      </div>
-      <div className='category-container'>
-        {/*<img /> */}
-        <div className='category-body-container'>
-          <h2>JACKETS</h2>
-          <p>SHOP NOW</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/*<img /> */}
-        <div className='category-body-container'>
-          <h2>SNEAKERS</h2>
-          <p>SHOP NOW</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/*<img /> */}
-        <div className='category-body-container'>
-          <h2>WOMENS</h2>
-          <p>SHOP NOW</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/*<img /> */}
-        <div className='category-body-container'>
-          <h2>MENS</h2>
-          <p>SHOP NOW</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
