ac5750fc2d01408c932e5439fed13ae7a30e33fc
Qibin <qbye0405@gmail.com>
2023-08-10 16:12:28 -0400

    added logo and styles but css is na

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,15 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
+
 function Navigation() {
   return (
     <Fragment>
-      <div className="nagivation-container">
+      <div className="nagivation">
         <Link className="logo-container" to="/">
-          <div>logo</div>
+          <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
